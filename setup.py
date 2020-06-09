300abf64783be24f88233381ebab64527dcae6e4
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2020-06-09 13:52:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,3 @@ setup(
     name="env_pkg",
     version="0.0"
 )
-
-
-
-##Note: this does *NOT* check dependencies
-##For dependencies, include e.g.:
-"""
-from setuptools import find_packages
-
-setup(
-    ...
-    install_requires=["gym>=0.2.3", "pandas", "cfg_load"],
-    packages=find_packages(),
-    ..
-)
-"""
