7cd564ac58162b5a0ac95bd8b5bd64b7af388f2c
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2020-06-09 13:39:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
 )
 
-/*Note, this does *NOT* check dependencies, include e.g.:
+"""Note, this does *NOT* check dependencies, include e.g.:
 ```
 from setuptools import find_packages
 
@@ -17,4 +17,4 @@ setup(
     packages=find_packages(),
     ..
 )
-```*/
+```"""
