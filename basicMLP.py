2d9aa0fee9df4e03e57d604acc04980b19949a5e
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2020-06-10 11:43:50 -0700

    Update basicMLP.py

--- basicMLP.py
+++ basicMLP.py
@@ -33,14 +33,14 @@ fl1 = Flatten()(inp)
 dn1 = Dense(100, activation='relu')(fl1)
 for i in range(n_cars):
   car_layer["dnR"+str(i)] = Reshape([1,n_acts])(Dense(n_acts)(dn1))
-DQNModel = Model(input=inp, output=Concatenate(axis=1)([car_layer[x] for x in car_layer])) #Current output (n_cars x n_acts)
+modelDQN = Model(input=inp, output=Concatenate(axis=1)([car_layer[x] for x in car_layer])) #Current output (n_cars x n_acts)
                                                                                            #Can also have n_cars length vector with each position giving the car's action number
                                                                                            #output = Dense(n_cars, activation='linear')(dn1)
 
 memory = SequentialMemory(limit=50000, window_length=1)
 policy = BoltzmannQPolicy()
 
-agentDQN = DQNAgent(model=DQNModel, nb_actions=n_acts, memory=memory, nb_steps_warmup=10,
+agentDQN = DQNAgent(model=modelDQN, nb_actions=n_acts, memory=memory, nb_steps_warmup=10,
                     target_model_update=1e-2, policy=policy)
 agentDQN.compile(Adam(lr=1e-3), metrics=['mae'])
 agentDQN.fit(env, nb_steps=10000, visualize=False)
