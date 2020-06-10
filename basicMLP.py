b26baff1b9b9af25c8d68f788133e5e0a747adac
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2020-06-09 21:23:48 -0700

    Update basicMLP.py
    Updated Architecture dimensions for MonoAgent

--- basicMLP.py
+++ basicMLP.py
@@ -3,7 +3,7 @@
 import gym
 import tensorflow as tf
 from keras.models import Input, Model
-from keras.layers import Flatten, Dense, Concatenate
+from keras.layers import Flatten, Dense, Concatenate, Reshape
 from keras.optimizers import Adam
 from keras import backend as K
 
@@ -19,21 +19,22 @@ cfg.gpu_options.allow_growth = True
 env = gym.make('fooEnv_ID')
 env.__init__(n_cars, n_acts, min_obs, max_obs, n_nodes, n_features)
 
+car_layer = {}
 
 #Architecture, simple feed-forward dense net
 inp = Input(((1,) + env.observation_space.shape))
 fl1 = Flatten()(inp)
 dn1 = Dense(100, activation='relu')(fl1)
-dn2 = Dense(100, activation='relu')(dn1)
-otp = Dense(n_cars, activation='linear')(dn2) #Current output a 
-                                              #Can also have n_cars length vector with each position giving the car's action number
-                                              #otp = Dense(n_cars, activation='linear')(dn2)
-DQNModel = Model(input=inp, output=otp)
+for i in range(n_cars):
+  car_layer["dnR"+str(i)] = Reshape([1,n_acts])(Dense(n_acts)(dn1))
+DQNModel = Model(input=inp, output=Concatenate(axis=1)([car_layer[x] for x in car_layer])) #Current output (n_cars x n_acts)
+                                                                                           #Can also have n_cars length vector with each position giving the car's action number
+                                                                                           #output = Dense(n_cars, activation='linear')(dn1)
 
 memory = SequentialMemory(limit=50000, window_length=1)
 policy = BoltzmannQPolicy()
 
-agentDQN = DQNAgent(model=DQNModel, nb_actions=n_cars, memory=memory, nb_steps_warmup=10,
+agentDQN = DQNAgent(model=DQNModel, nb_actions=n_acts, memory=memory, nb_steps_warmup=10,
                     target_model_update=1e-2, policy=policy)
 agentDQN.compile(Adam(lr=1e-3), metrics=['mae'])
 agentDQN.fit(env, nb_steps=10000, visualize=False)
