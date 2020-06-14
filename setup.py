31c1306ac638411d94ea9c6f854a692b03916665
Peter Ott <ott.peter.r@gmail.com>
2020-06-14 10:50:00 -0400

    adding dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,14 @@ from setuptools import setup
 
 setup(
     name="env_pkg",
-    version="0.0"
+    version="0.0",
+    install_requires=[
+          'tensorflow==2.2.0',
+          'keras-rl==0.4.2',
+          'flatland-rl==2.2.1',
+          'numpy==1.18.5',
+          'gym==0.14.0'
+      ],
+
 )
+
